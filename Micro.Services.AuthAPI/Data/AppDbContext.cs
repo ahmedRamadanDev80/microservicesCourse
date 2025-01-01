0b4a2d3b00d152d1297f3a4b22cd495a1c9b71d9
altair <ar80pro@gmail.com>
2025-01-01 13:34:23 +0200

    Add Custom Properties to Users Table

--- Micro.Services.AuthAPI/Data/AppDbContext.cs
+++ Micro.Services.AuthAPI/Data/AppDbContext.cs
@@ -1,14 +1,17 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Micro.Services.AuthAPI.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Micro.Services.AuthAPI.Data
 {
-    public class AppDbContext : IdentityDbContext
+    public class AppDbContext : IdentityDbContext<ApplicationUser>
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
         }
-        
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
