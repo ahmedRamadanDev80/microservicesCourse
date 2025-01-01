e9505cc2cf04b3069b19211da34f8101f260b7d3
altair <ar80pro@gmail.com>
2025-01-01 14:49:53 +0200

    IAuth Service

---
+++ Micro.Services.AuthAPI/Services/IService/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using Micro.Services.AuthAPI.Models.Dtos;
+
+namespace Micro.Services.AuthAPI.Services.IService
+{
+    public interface IAuthService
+    {
+        Task<UserDto> Register(RegistrationRequestDto registrationRequestDto);
+        Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto);
+    }
+}
