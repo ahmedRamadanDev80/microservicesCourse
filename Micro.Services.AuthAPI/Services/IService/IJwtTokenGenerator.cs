60660d800ef5d81a53cd98dd6c9ffc4e19d1c4d3
Ahmed.Mubarak <ar80pro@gmail.com>
2025-09-23 12:36:18 +0300

    Generate Jwt Token

---
+++ Micro.Services.AuthAPI/Services/IService/IJwtTokenGenerator.cs
@@ -0,0 +1,10 @@
+﻿using Micro.Services.AuthAPI.Models;
+using Micro.Services.AuthAPI.Models.Dtos;
+
+namespace Micro.Services.AuthAPI.Services.IService
+{
+    public interface IJwtTokenGenerator
+    {
+        string GenerateToken(ApplicationUser applicationUser);
+    }
+}
---
+++ Micro.Services.AuthAPI/Services/JwtTokenGenerator.cs
@@ -0,0 +1,15 @@
+﻿using Micro.Services.AuthAPI.Models;
+using Micro.Services.AuthAPI.Models.Dtos;
+using Micro.Services.AuthAPI.Services.IService;
+
+namespace Micro.Services.AuthAPI.Services
+{
+    public class JwtTokenGenerator : IJwtTokenGenerator
+    {
+        public string GenerateToken(ApplicationUser applicationUser)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
