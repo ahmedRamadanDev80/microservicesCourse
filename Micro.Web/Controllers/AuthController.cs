aa65744ccdc36d3c4322c4598fc44ce79275364d
altair <ar80pro@gmail.com>
2025-09-26 21:25:45 +0300

    Validation with Login and Register

--- Micro.Web/Controllers/AuthController.cs
+++ Micro.Web/Controllers/AuthController.cs
@@ -45,7 +45,7 @@ namespace Micro.Web.Controllers
             }
             else
             {
-                ModelState.AddModelError("CustomError", responseDto.Message);
+                TempData["error"] = responseDto.Message;
                 return View(obj);
             }
         }
@@ -81,6 +81,10 @@ namespace Micro.Web.Controllers
                     TempData["success"] = "Registration Successful";
                     return RedirectToAction(nameof(Login));
                 }
+            }
+            else
+            {
+                TempData["error"] = result.Message;
             }
 
             var roleList = new List<SelectListItem>()
--- Micro.Web/Models/LoginResponseDto.cs
+++ Micro.Web/Models/LoginResponseDto.cs
@@ -1,8 +1,12 @@
-﻿namespace Micro.Web.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Micro.Web.Models
 {
     public class LoginResponseDto
     {
+        [Required]
         public UserDto User { get; set; }
+        [Required]
         public string Token { get; set; }
     }
 }
