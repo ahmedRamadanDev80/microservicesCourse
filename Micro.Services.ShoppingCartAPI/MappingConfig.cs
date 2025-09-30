cc9c9120f006d82740edba048a2a023ea0dd8280
Ahmed.Mubarak <ar80pro@gmail.com>
2025-09-30 19:39:39 +0300

    Basic API Setup

---
+++ Micro.Services.ShoppingCartAPI/MappingConfig.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Micro.Services.ShoppingCartAPI.Models;
+using Micro.Services.ShoppingCartAPI.Models.Dtos;
+
+namespace Micro.Services.ShoppingCartAPI
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                config.CreateMap<CartHeader, CartHeaderDto>().ReverseMap();
+                config.CreateMap<CartDetails, CartDetailsDto>().ReverseMap();
+            });
+            return mappingConfig;
+        }
+    }
+}
