0c77aa95a01427b45b949364b5f116c0593e18bd
altair <ar80pro@gmail.com>
2025-10-05 20:23:44 +0300

    Register Service Bus Consumer on Application Start

--- Micro.Services.EmailAPI/Program.cs
+++ Micro.Services.EmailAPI/Program.cs
@@ -1,5 +1,7 @@
 
 using Micro.Services.EmailAPI.Data;
+using Micro.Services.EmailAPI.Extension;
+using Micro.Services.EmailAPI.Messaging;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +11,9 @@ builder.Services.AddDbContext<AppDbContext>(option =>
 {
     option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+builder.Services.AddSingleton<IAzureServiceBusConsumer, AzureServiceBusConsumer>();
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -29,6 +34,7 @@ app.UseAuthorization();
 
 app.MapControllers();
 ApplyMigration();
+app.UseAzureServiceBusConsumer();
 app.Run();
 
 
