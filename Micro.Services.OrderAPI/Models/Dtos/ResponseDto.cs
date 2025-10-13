561210e51ed8513d7d10ef5f01e231110dd09024
Ahmed.Mubarak <ar80pro@gmail.com>
2025-10-13 14:03:35 +0300

    Add DTO's in Order API

---
+++ Micro.Services.OrderAPI/Models/Dtos/ResponseDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Micro.Services.OrderAPI.Models.Dtos
+{
+    public class ResponseDto
+    {
+
+        public object? Result { get; set; }
+        public bool IsSuccess { get; set; } = true;
+        public string Message { get; set; } = "";
+    }
+}
