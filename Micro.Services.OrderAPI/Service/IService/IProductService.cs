f5ae7fad932e4e5a6d02990baac4a9427798f755
Ahmed.Mubarak <ar80pro@gmail.com>
2025-10-13 14:24:59 +0300

    Order API Base Setup

---
+++ Micro.Services.OrderAPI/Service/IService/IProductService.cs
@@ -0,0 +1,9 @@
+﻿using Micro.Services.OrderAPI.Models.Dtos;
+
+namespace Micro.Services.OrderAPI.Service.IService
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductDto>> GetProducts();
+    }
+}
